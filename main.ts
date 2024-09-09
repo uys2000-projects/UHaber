37b57fecb50d4950689e3a096e212cffeb096c20
Mehmet Uysal <siliente2020@gmail.com>
2024-09-09 09:39:43 +0300

    update

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@ import "./src/ulogger";
 
 (async () => {
   if (process.argv[2] == "update") (await import("./src/source")).default();
-  else if (process.argv[2] == "start") (await import("./src/cron")).default();
-  else if (process.argv[2] == "addNews") (await import("./src/news")).default();
+  else if (process.argv[2] == "start") {
+    (await import("./src/news")).default();
+    (await import("./src/cron")).default();
+  } else if (process.argv[2] == "addNews")
+    (await import("./src/news")).default();
 })();
